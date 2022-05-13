2bee8e0915952bd3d7f3d0d65d9cfdaa70e19fad
mauro sala <mauro.sala@gmail.com>
2022-05-13 09:36:04 +0200

    fix(routes): name

--- routes/status.routes.js
+++ routes/status.routes.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/ping', (req, res) => {
-  res.status(200).send('Template Service')
+  res.status(200).send('Auth Service')
 })
 
 router.get('/healthz', (req, res) => {
-  res.status(200).send('Template Service')
+  res.status(200).send('Auth Service')
 })
 
 module.exports = router
