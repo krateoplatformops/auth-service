100372c8feb8a20fbc63f092156c493f290fd101
mauro sala <mauro.sala@gmail.com>
2022-10-26 12:06:57 +0200

    fix(read): logging

--- controllers/strategy/read.strategy.controller.js
+++ controllers/strategy/read.strategy.controller.js
@@ -3,10 +3,13 @@ const router = express.Router()
 const k8sHelpers = require('../../service-library/helpers/k8s.helpers')
 const { k8sConstants } = require('../../service-library/constants')
 const responseHelpers = require('../../helpers/response.helpers')
+const logger = require('../../service-library/helpers/logger.helpers')
 
 router.get('/', async (req, res, next) => {
   try {
     const list = await k8sHelpers.getList(k8sConstants.strategyApi)
+
+    logger.debug(k8sConstants.strategyApi)
 
     res.status(200).json({ list: list.map((x) => responseHelpers.parse(x)) })
   } catch (error) {
