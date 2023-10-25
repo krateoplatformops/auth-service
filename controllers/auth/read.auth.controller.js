d8c88ba398b5aa655b3de47b6077e75e78dec939
lucabertelli <bertelli.l86@gmail.com>
2023-10-25 10:42:40 +0200

    fix(github): fix github enterprise authentication

--- controllers/auth/read.auth.controller.js
+++ controllers/auth/read.auth.controller.js
@@ -38,117 +38,119 @@ router.get('/guest', async (req, res, next) => {
   }
 })
 
+router.get('/github', passport.authenticate('github', { scope: ['read:user'] }))
+
 router.get(
-  '/github',
-  passport.authenticate('github', { scope: ['user:email'] })
-)
-
-router.get('/github/callback', async (req, res, next) => {
-  logger.debug('1')
-  let strategy = null
-  try {
-    strategy = await k8sHelpers.getSingleByName(
-      k8sConstants.strategyApi,
-      'github'
-    )
-  } catch (error) {
-    next(error)
-  }
-
-  logger.debug('2')
-  logger.debug(strategy)
-
-  if (!strategy) {
-    const err = new Error('Cannot find strategy')
-    err.statusCode = 500
-    next(err)
-    return
-  }
-
-  const provider = responseHelpers.parse(strategy, true)
-
-  logger.debug('3')
-  logger.debug(provider)
-
-  if (!provider) {
-    const err = new Error('Unknown authentication strategy')
-    err.statusCode = 500
-    next(err)
-    return
-  }
-
-  const config = JSON.parse(stringHelpers.b64toAscii(provider.spec.config))
-
-  logger.debug('4')
-  logger.debug(config)
-  logger.debug(req)
-  const grantCode = req.query.code
-
-  const tokenURL = config.tokenURL
-  const userProfileURL = config.userProfileURL
-  const clientId = config.clientID
-  const clientSecret = config.clientSecret
-  const userInfo = {}
-
-  logger.debug('----> new UserInfo')
-
-  let accessToken = null
-
-  fetch(
-    tokenURL +
-      '?client_id=' +
-      clientId +
-      '&client_secret=' +
-      clientSecret +
-      '&code=' +
-      grantCode,
-    {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json'
-      }
+  '/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  async function (req, res, next) {
+    // Successful authentication, redirect home.
+    logger.debug('1')
+    let strategy = null
+    try {
+      strategy = await k8sHelpers.getSingleByName(
+        k8sConstants.strategyApi,
+        'github'
+      )
+    } catch (error) {
+      next(error)
     }
-  )
-    .then((respToken) => respToken.json())
-    .then((jsonToken) => {
-      logger.debug('5')
-      accessToken = jsonToken.access_token
-    })
-    .catch((err) => console.log(err))
-    .then(() => {
-      fetch(userProfileURL, {
-        method: 'GET',
+
+    logger.debug('2')
+    logger.debug(strategy)
+
+    if (!strategy) {
+      const err = new Error('Cannot find strategy')
+      err.statusCode = 500
+      next(err)
+      return
+    }
+
+    const provider = responseHelpers.parse(strategy, true)
+
+    logger.debug('3')
+    logger.debug(provider)
+
+    if (!provider) {
+      const err = new Error('Unknown authentication strategy')
+      err.statusCode = 500
+      next(err)
+      return
+    }
+
+    const config = JSON.parse(stringHelpers.b64toAscii(provider.spec.config))
+
+    logger.debug('4')
+    logger.debug(config)
+    logger.debug(req)
+    const grantCode = req.query.code
+
+    const tokenURL = config.tokenURL
+    const userProfileURL = config.userProfileURL
+    const clientId = config.clientID
+    const clientSecret = config.clientSecret
+    const userInfo = {}
+
+    logger.debug('----> new UserInfo')
+
+    let accessToken = null
+
+    fetch(
+      tokenURL +
+        '?client_id=' +
+        clientId +
+        '&client_secret=' +
+        clientSecret +
+        '&code=' +
+        grantCode,
+      {
+        method: 'POST',
         headers: {
-          Accept: 'application/json',
-          Authorization: 'Bearer ' + accessToken
+          Accept: 'application/json'
         }
+      }
+    )
+      .then((respToken) => respToken.json())
+      .then((jsonToken) => {
+        logger.debug('5')
+        accessToken = jsonToken.access_token
       })
-        .then((respUser) => respUser.json())
-        .then((json) => {
-          logger.debug(clientSecret)
-          userInfo.id = json.id
-          userInfo.displayName = json.name
-          userInfo.username = json.login
-          userInfo.email = json.email
-
-          logger.info('8')
-          logger.info(JSON.stringify(userInfo))
-          const user = authHelpers.cookie(userInfo, 'github')
-
-          logger.info('9')
-          logger.info(user)
-
-          res.cookie(
-            envConstants.COOKIE_NAME,
-            jwtHelpers.sign(user),
-            cookieConstants
-          )
-          res.redirect(global.redirect)
-          res.status(200)
+      .catch((err) => console.log(err))
+      .then(() => {
+        fetch(userProfileURL, {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + accessToken
+          }
         })
-        .catch((err) => console.log(err))
-    })
-})
+          .then((respUser) => respUser.json())
+          .then((json) => {
+            logger.debug(clientSecret)
+            userInfo.id = json.id
+            userInfo.displayName = json.name
+            userInfo.username = json.login
+            userInfo.email = json.email
+
+            logger.info('8')
+            logger.info(JSON.stringify(userInfo))
+            const user = authHelpers.cookie(userInfo, 'github')
+
+            logger.info('9')
+            logger.info(user)
+
+            res.cookie(
+              envConstants.COOKIE_NAME,
+              jwtHelpers.sign(user),
+              cookieConstants
+            )
+            res.redirect(global.redirect)
+            res.status(200)
+          })
+          .catch((err) => console.log(err))
+      })
+  }
+)
 
 router.get(
   '/microsoft',
